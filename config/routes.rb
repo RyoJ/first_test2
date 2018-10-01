c1e51687f12650f2908e6b12449cc32eb9d890de
RyoShelf3 <ec2-user@ip-172-31-47-53.us-east-2.compute.internal>
2018-10-01 02:39:15 +0000

    pre release

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  get 'cards/index'
+  get 'cards', to: 'cards#index'
+
+  get 'cards/add'
+  post 'cards/add'
+
+  get 'cards/:id', to: 'cards#show'
+
+  get 'cards/edit/:id', to: 'cards#edit'
+  patch 'cards/edit/:id', to: 'cards#edit'
+  
+  get 'cards/delete/:id', to: 'cards#delete'
+  
+  root 'cards#index'
 end
